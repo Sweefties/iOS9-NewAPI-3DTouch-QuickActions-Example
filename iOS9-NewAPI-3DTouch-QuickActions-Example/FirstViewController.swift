6e8a499a249f3e77d06700849e9a35f8ccf7b7a4
Wlad Dicario <wlad@2v-prod.com>
2015-09-23 03:59:03 +0200

    Initial Commit

---
+++ iOS9-NewAPI-3DTouch-QuickActions-Example/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iOS9-NewAPI-3DTouch-QuickActions-Example
+//
+//  Created by Wlad Dicario on 23/09/2015.
+//  Copyright © 2015 Sweefties. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS9-NewAPI-3DTouch-QuickActions-Example/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS9-NewAPI-3DTouch-QuickActions-Example
+//
+//  Created by Wlad Dicario on 23/09/2015.
+//  Copyright © 2015 Sweefties. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
